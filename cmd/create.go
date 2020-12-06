07940b756059ca9c677ed0acae4a901e1a8dc4e4
shuheitakada <s-takada@giftee.co>
2020-12-06 16:09:22 +0900

    Fix: テストしやすいように、rootCmdとcreateCmdを関数化

--- cmd/create.go
+++ cmd/create.go
@@ -1,47 +1,16 @@
 package cmd
 
 import (
-	"encoding/csv"
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
-// createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create a csv file",
-	Run: func(cmd *cobra.Command, args []string) {
-		file, err := os.OpenFile("tmp/example.csv", os.O_WRONLY|os.O_CREATE, 0600)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer file.Close()
-
-		err = file.Truncate(0)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		writer := csv.NewWriter(file)
-		for i := 0; i < 100; i++ {
-			writer.Write([]string{"hoge"})
-		}
-		writer.Flush()
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(createCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func NewCmdCreate() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a csv file",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Println("create is called")
+		},
+	}
+	return cmd
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,37 +1,26 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "eris",
-	Short: "CLI to create a csv file",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+func NewCmdRoot() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "eris",
+		Short: "CLI to create a csv file",
+	}
+	cmd.AddCommand(NewCmdCreate())
+	return cmd
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	cmd := NewCmdRoot()
+	cmd.SetOutput(os.Stdout)
+	if err := cmd.Execute(); err != nil {
+		cmd.SetOutput(os.Stderr)
+		cmd.Println(err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.eris.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
