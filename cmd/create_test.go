c0fdafa6fbf4e5dcc78ec04d86c5bec1c922c9d6
shuheitakada <s-takada@giftee.co>
2020-12-13 15:50:38 +0900

    Add: ランダムな数値を生成するコマンドのテストを追加

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -1,19 +1,36 @@
 package cmd
 
 import (
-	"bytes"
+	"io/ioutil"
+	"os"
+	"regexp"
 	"testing"
 )
 
+// eris create "/\d{4}/" -n 3 --file {{tmpfile}}
 func TestCreate(t *testing.T) {
-	want := "create is called\n"
-	buf := new(bytes.Buffer)
+	tmp, err := ioutil.TempFile("./", "tmp")
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+	defer func() {
+		os.Remove(tmp.Name())
+	}()
 	cmd := NewCmdCreate()
-	cmd.SetOutput(buf)
+	cmd.SetArgs([]string{`/\d{4}/`, "-n", "3", "--file", tmp.Name()})
 	cmd.Execute()
-	got := buf.String()
 
-	if got != want {
+	tmpfile, _ := os.Open(tmp.Name())
+	defer tmpfile.Close()
+
+	tmpContent, err := ioutil.ReadAll(tmpfile)
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+
+	got := string(tmpContent)
+	want := `\A(\d{4}\n)+\z`
+	if regexp.MustCompile(want).MatchString(got) == false {
 		t.Errorf("\nwant: %#v\ngot: %#v", want, got)
 	}
 }
